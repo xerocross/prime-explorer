8801eb3f95ddefec92b55452135572a2aa807875
xerocross <adam.cross.inc@gmail.com>
2018-12-07 19:04:08 -0500

    cleanup

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
-const spawn = require("child_process").spawn;
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
-var checkEquality = function (f1, f2, callback) {
-    let pythonProcess = spawn('python3', ["pyscripts/checkEquality.py", f1, f2]);
-    pythonProcess.stdout.on('data', callback);
-}
 
 app.use(express.static('public'))
 app.use('/dist', express.static('dist'))
