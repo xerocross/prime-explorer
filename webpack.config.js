b155b5671b75cedfded4ae29e4b811db97b778ba
xerocross <adam.cross.inc@gmail.com>
2018-12-17 12:37:00 -0500

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         path : path.resolve(__dirname, './dist'),
         publicPath : '/dist',
         filename : '[name].js',
-        library : 'CrossPrimeExplorer',
+        library : 'PrimeExplorer',
         libraryTarget : 'umd',
         umdNamedDefine : true
     },
